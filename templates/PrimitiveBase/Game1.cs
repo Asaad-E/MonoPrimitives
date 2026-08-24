c824bb97fb05b738806fc22c98130695b43c4e7e
Asaad-E <asaadeveloper@gmail.com>
2026-08-24 14:27:45 -0500

    refactor: template -- file-scoped namespace, inline RenderContext, window title, PrimitiveInput(Window)

--- templates/PrimitiveBase/Game1.cs
+++ templates/PrimitiveBase/Game1.cs
@@ -4,83 +4,92 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoPrimitives;
 using MonoPrimitives.Primitives2D;
 
-namespace PrimitiveBase
+namespace PrimitiveBase;
+
+public readonly record struct RenderContext(
+        GraphicsDevice GraphicsDevice,
+        GraphicsDeviceManager Graphics,
+        SpriteBatch SpriteBatch,
+        Primitive2DBatch Batch2D);
+
+
+public class Game1 : Game
 {
-    public class Game1 : Game
+    private const string Title = "Base Template";
+    private const int VirtualWidth = 1920;
+    private const int VirtualHeight = 1080;
+    private const int TargetFps = 60;
+
+    private readonly GraphicsDeviceManager _graphics;
+    private ViewportAdapter2D _viewportAdapter;
+    private Camera2D _camera;
+    private PrimitiveInput _input;
+    private RenderContext _render;
+
+    public Game1()
     {
-        private const int VirtualWidth = 1920;
-        private const int VirtualHeight = 1080;
-        private const int TargetFps = 60;
-
-        private readonly GraphicsDeviceManager _graphics;
-        private ViewportAdapter2D _viewportAdapter;
-        private Camera2D _camera;
-        private PrimitiveInput _input;
-        private RenderContext _render;
-
-        public Game1()
+        _graphics = new GraphicsDeviceManager(this)
         {
-            _graphics = new GraphicsDeviceManager(this)
-            {
-                PreferredBackBufferWidth = 1280,
-                PreferredBackBufferHeight = 720,
-                PreferMultiSampling = true,
-            };
-            _graphics.PreparingDeviceSettings += (sender, e) =>
-            {
-                e.GraphicsDeviceInformation.PresentationParameters.MultiSampleCount = 4;
-            };
-
-            Window.AllowUserResizing = true;
-
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-
-            IsFixedTimeStep = true;
-            TargetElapsedTime = TimeSpan.FromSeconds(1.0 / TargetFps);
-        }
-
-        protected override void Initialize()
+            PreferredBackBufferWidth = 1280,
+            PreferredBackBufferHeight = 720,
+            PreferMultiSampling = true,
+        };
+        _graphics.PreparingDeviceSettings += (sender, e) =>
         {
+            e.GraphicsDeviceInformation.PresentationParameters.MultiSampleCount = 4;
+        };
 
-            _viewportAdapter = new BoxingViewportAdapter2D(GraphicsDevice, VirtualWidth, VirtualHeight);
-            _camera = new Camera2D(_viewportAdapter) { Offset = Vector2.Zero };
-            _input = new PrimitiveInput();
+        Window.AllowUserResizing = true;
+        Window.Title = Title;
 
-            base.Initialize();
-        }
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
 
-        protected override void LoadContent()
-        {
-            _render = new RenderContext(
-                GraphicsDevice,
-                _graphics,
-                new SpriteBatch(GraphicsDevice),
-                new Primitive2DBatch(GraphicsDevice));
-        }
+        IsFixedTimeStep = true;
+        TargetElapsedTime = TimeSpan.FromSeconds(1.0 / TargetFps);
+    }
 
-        protected override void Update(GameTime gameTime)
-        {
-            _input.Update(gameTime);
+    protected override void Initialize()
+    {
 
-            if (_input.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
-                Exit();
+        _viewportAdapter = new BoxingViewportAdapter2D(GraphicsDevice, VirtualWidth, VirtualHeight);
+        _camera = new Camera2D(_viewportAdapter) { Offset = Vector2.Zero };
+        _input = new PrimitiveInput(Window);
 
-            base.Update(gameTime);
-        }
+        base.Initialize();
+    }
 
-        protected override void Draw(GameTime gameTime)
-        {
-            _render.Batch2D.ClearLetterboxed(_viewportAdapter, backgroundColor: Palette.Background);
+    protected override void LoadContent()
+    {
+        _render = new RenderContext(
+            GraphicsDevice,
+            _graphics,
+            new SpriteBatch(GraphicsDevice),
+            new Primitive2DBatch(GraphicsDevice));
+    }
 
-            _render.Batch2D.Begin(_camera.GetTransformMatrix());
+    protected override void Update(GameTime gameTime)
+    {
+        _input.Update(gameTime);
 
-            var (inner, outer) = Palette.GradientPairs[0];
-            _render.Batch2D.FillCircleGradient(new Vector2(VirtualWidth / 2f, VirtualHeight / 2f), 150f, inner, outer);
+        if (_input.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
+            Exit();
 
-            _render.Batch2D.End();
+        base.Update(gameTime);
+    }
 
-            base.Draw(gameTime);
-        }
+    protected override void Draw(GameTime gameTime)
+    {
+        _render.Batch2D.ClearLetterboxed(_viewportAdapter, backgroundColor: Palette.Background);
+
+        _render.Batch2D.Begin(_camera.GetTransformMatrix());
+
+        var (inner, outer) = Palette.GradientPairs[0];
+        _render.Batch2D.FillCircleGradient(new Vector2(VirtualWidth / 2f, VirtualHeight / 2f), 150f, inner, outer);
+
+        _render.Batch2D.End();
+
+        base.Draw(gameTime);
     }
 }
+
